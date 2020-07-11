86fd3d19426b5f79af63202d53ea8234648d9e9a
wj12580 <2042943518@qq.com>
2020-07-11 09:15:08 +0800

    东方的秘密

---
+++ src/main/java/dongfang.java
@@ -0,0 +1,9 @@
+/**
+ * @author wj
+ * @create 2020-07-11 9:12
+ */
+public class dongfang {
+    public static void main(String[] args) {
+        System.out.println("wozui xinhuanderen");
+    }
+}

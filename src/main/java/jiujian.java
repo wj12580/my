83d62a5b91a88075e62cf27eb946528dc0d1845b
wj12580 <2042943518@qq.com>
2020-07-11 09:18:17 +0800

    这是个武林秘籍

--- src/main/java/jiujian.java
+++ src/main/java/jiujian.java
@@ -5,5 +5,7 @@
 public class jiujian {
     public static void main(String[] args) {
         System.out.println("sssss");
+        System.out.println("jsi");
+        //红色未追踪代码  绿色未追踪代码首次进暂存区 黑色已提交到本地库 黑色已提交本地库后又发生修改且未提交
     }
 }
